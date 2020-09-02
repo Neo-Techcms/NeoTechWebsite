5dee62e97b3510b483004e7b50132e79e3a3403e
Ayush <ayush.keshav2004@gmail.com>
2020-09-02 18:52:41 +0530

    Changes

---
+++ js/intro.js
@@ -0,0 +1,26 @@
+
+t1 =  setTimeout(function()
+{ 
+  document.getElementById("maintext").innerHTML = "I am Ayush";
+}, 3000);
+
+t2 =  setTimeout(function()
+{ 
+  document.getElementById("maintext").innerHTML = "Let me Introduce You To Pen";
+}, 6000);
+
+t3 =  setTimeout(function()
+{ 
+  document.getElementById("maintext").innerHTML = "A Social Media Platform";
+}, 9000);
+
+t4 =  setTimeout(function()
+{ 
+  document.getElementById("maintext").innerHTML = "Made In India";
+}, 12000);
+
+
+t4 =  setTimeout(function()
+{ 
+  window.location = "./logsin.html";
+}, 15000);
